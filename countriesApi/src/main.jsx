fab1a13c6e88eea571c579ea16fc7a6cbc9c748c
Alexander Celis <alexcelis019@gmail.com>
2022-08-18 21:28:41 -0500

    create router component to app

--- countriesApi/src/main.jsx
+++ countriesApi/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 // styles
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -8,31 +8,12 @@ import './styles/index.css'
 
 // components
 // import App from './components/App'
-import App from './pages/App'
-import Dashboard from './pages/Dashboard'
-import Contact from './pages/Contact'
-import Navbar from './components/Navbar'
+import Router from './router'
 
 createRoot(document.getElementById('app')).render(
   <StrictMode>
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/dashboard' element={<Dashboard />}>
-          {/* <Route index element={<Dashboard />} /> */}
-          <Route path='store' element={<p>Store</p>} />
-        </Route>
-        <Route path='/contact' element={<Contact />} />
-        <Route
-          path='*'
-          element={<Navigate to='/404' replace />}
-        />
-        <Route
-          path='/404'
-          element={<p className='container'>Page not found - 404</p>}
-        />
-      </Routes>
+      <Router />
     </BrowserRouter>
   </StrictMode>
 )
---
+++ countriesApi/src/router.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom'
+import App from './components/App'
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<App />} />
+    </Routes>
+  )
+}
+
+export default Router
